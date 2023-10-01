b3f0d214609b70ca8615bbda7bf329af173a4cca
GUI python project <kahano243@gmail.com>
2023-10-02 00:32:48 +0200

    new test file added

---
+++ src/test/java/com/project/student_management_system/studentServiceTest.java
@@ -0,0 +1,56 @@
+package com.project.student_management_system;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.project.student_management_system.entity.student;
+import com.project.student_management_system.repository.studentRepository;
+import com.project.student_management_system.service.studentServiceImp.studentServiceImpl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class studentServiceTest{
+
+    @Mock
+    private studentRepository student_repository;
+
+    @InjectMocks
+    private studentServiceImpl student_service;
+
+    
+     
+    @Test
+    public void get_listOfStudents(){
+        
+      when(student_repository.findAll()).thenReturn(Arrays.asList
+        (new student("John", "Bjørnson", "john_bj@gmail.com"),
+        new student("Andreas", "Karlsen", "And_karl@gmail.com")));
+      
+
+        List<student> result = student_service.getAllstudents();
+
+        assertEquals("John",result.get(0).getFirstname());
+        assertEquals("Bjørnson",result.get(0).getLastname());
+        
+        assertEquals("Andreas",result.get(1).getFirstname());
+        assertEquals("Karlsen",result.get(1).getLastname());
+       
+    }
+     
+   
+
+   
+
+
+    
+    
+}
