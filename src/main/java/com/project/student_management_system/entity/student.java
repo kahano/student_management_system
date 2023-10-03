2e34d24aec1c6990f6643ab85f5611dc0627bdb0
GUI python project <kahano243@gmail.com>
2023-10-04 00:23:30 +0200

    refactoring-lombok

--- src/main/java/com/project/student_management_system/entity/student.java
+++ src/main/java/com/project/student_management_system/entity/student.java
@@ -9,12 +9,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Entity
 @Table(name = "students")
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 
